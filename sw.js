df86acbc61fbd45be8dede598c5f6aeca502e4d5
Mouhammed-AldarwisH <slap4915@gmail.com>
2025-08-06 17:56:38 +0300

    app

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('kdamk-cache').then(function(cache) {
+      return cache.addAll([
+        '/kdamk1/login.html',
+        '/kdamk1/style.css', // عدّل حسب اسم ملف CSS لو عندك
+        '/kdamk1/script.js'  // عدّل حسب ملف JS لو عندك
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
