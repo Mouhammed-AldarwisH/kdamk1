7a88fac2635e420458dc82c926b8e08a37ce5f76
Mouhammed-AldarwisH <146646599+Mouhammed-AldarwisH@users.noreply.github.com>
2025-08-13 21:06:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('kdamk-cache').then(function(cache) {
       return cache.addAll([
-        '/kdamk1/login.html',
+        '/kdamk1/create-request.html',
         '/kdamk1/style.css', // عدّل حسب اسم ملف CSS لو عندك
         '/kdamk1/script.js'  // عدّل حسب ملف JS لو عندك
       ]);
